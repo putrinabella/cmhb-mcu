4c3bae12d8ba2b6193c5ffc3ee1e21508b09b521
Putri Nabella <putrinabellaaa@gmail.com>
2025-08-27 20:17:56 +0800

    feat: zoom on pdf

--- src/layout/PhoneLayout.tsx
+++ src/layout/PhoneLayout.tsx
@@ -9,7 +9,6 @@ export default function PhoneLayout() {
 
   const navItems = [
     // { path: "/company-profile", icon: Building2, label: "Perusahaan" },
-    // { path: "/registrasi-karyawan", icon: UserPlus, label: "Register" },
     { path: "/hasil-mcu", icon: HeartPulse, label: "MCU" },
     { path: "/dashboard", icon: House, label: "Dashboard" },
     { path: "/profile", icon: User, label: "Profile" },
